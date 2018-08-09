fe4bbacbf9fa351a35618c90465d197799661dc0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-08 18:27:37 -0700

    Remarkable IP

--- src/ifs_ints_and_loops/Remarkable.java
+++ src/ifs_ints_and_loops/Remarkable.java
@@ -1,7 +1,7 @@
 package ifs_ints_and_loops;
 
 import javax.swing.JOptionPane;
-
+//
 public class Remarkable {
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Hello, Java coding class!");
--- src/ifs_ints_and_loops/Unbirthday.java
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -1,7 +1,7 @@
 package ifs_ints_and_loops;
 
 import javax.swing.JOptionPane;
-
+//
 public class Unbirthday {
 	public static void main(String[] args) {
 		String date = JOptionPane.showInputDialog("When is your birthday? (MM/DD)");
