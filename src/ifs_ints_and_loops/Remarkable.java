ba9114aab525d6cc00c81c501445dc8f4bf9b201
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-08 18:26:22 -0700

    Got started on module 1

---
+++ src/ifs_ints_and_loops/Remarkable.java
@@ -0,0 +1,18 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello, Java coding class!");
+		JOptionPane.showMessageDialog(null, "I'm here to tell you something remarka--");
+		JOptionPane.showMessageDialog(null, "Wait a minute...");
+		JOptionPane.showInputDialog(null, "Who are you?");
+		JOptionPane.showMessageDialog(null, "Oh, uh. That's embarassing.");
+		JOptionPane.showMessageDialog(null, "I've never heard of you.");
+		JOptionPane.showMessageDialog(null, "Well in that case, I guess I have to make up a fact, don't I?");
+		JOptionPane.showMessageDialog(null, "Or, better yet, you could...");
+		String animal = JOptionPane.showInputDialog("Give me an animal! ex. gorilla, hippo, dog");
+		String noun = JOptionPane.showInputDialog("Give me a weapon! ex. sword, crowbar");
+	}
+}
---
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -0,0 +1,14 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		String date = JOptionPane.showInputDialog("When is your birthday? (MM/DD)");
+		if (date.equals("08/08")) {
+			JOptionPane.showMessageDialog(null, "Have a happy birthday!");
+		} else {
+			JOptionPane.showMessageDialog(null, "Have a happy UNbirthday!");
+		}
+	}
+}
